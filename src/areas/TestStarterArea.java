af7a26f4b8d2d75d3e582da7fa6cd607bd3d796a
Dexter.Richards <Dexter.Richards@sovos.com>
2016-03-08 15:00:20 -0500

    Added another button action

--- src/areas/TestStarterArea.java
+++ src/areas/TestStarterArea.java
@@ -2,6 +2,7 @@ package areas;
 
 import controllers.ButtonTextChangeAction;
 import controllers.PrintAction;
+import controllers.ToggleButtonVisibilityAction;
 import screenareas.DisplayAreas;
 import screenareas.NavigationArea;
 import screenareas.NavigationArea.ButtonNames;
@@ -22,7 +23,12 @@ public class TestStarterArea extends Area {
 		area.registerAction(NavigationArea.ButtonNames.EAST, new PrintAction("east clicked", area.getTextArea(), PrintAction.PrintOptions.APPEND));
 		area.registerAction(NavigationArea.ButtonNames.WEST, new PrintAction("west clicked", area.getTextArea(), PrintAction.PrintOptions.APPEND));
 
-		area.registerAction(NavigationArea.ButtonNames.CENTER, new PrintAction("center clicked", area.getTextArea(), PrintAction.PrintOptions.APPEND));
-		area.registerAction(NavigationArea.ButtonNames.CENTER, new ButtonTextChangeAction("center clicked", area.getActionInfoArea().getNavigationArea().getButton(ButtonNames.CENTER)));
+		area.registerAction(NavigationArea.ButtonNames.CENTER,
+				new PrintAction("center clicked", area.getTextArea(), PrintAction.PrintOptions.APPEND));
+		area.registerAction(NavigationArea.ButtonNames.CENTER,
+				new ButtonTextChangeAction("center clicked", area.getActionInfoArea().getNavigationArea().getButton(ButtonNames.CENTER)));
+		
+		area.registerAction(NavigationArea.ButtonNames.NORTHWEST,
+				new ToggleButtonVisibilityAction(area.getActionInfoArea().getNavigationArea().getButton(NavigationArea.ButtonNames.SOUTHEAST)));
 	}
 }
